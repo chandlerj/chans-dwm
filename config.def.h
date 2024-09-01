2b6f1ac3883e9f20cf57df17fb164ece710aa284
Chandler J <cjustice2000@gmail.com>
2024-09-01 09:00:16 -0600

    most up to date config

--- config.def.h
+++ config.def.h
@@ -7,8 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 24;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "DM Mono:size=11" };
-static const char dmenufont[]       = "DM Mono:size=10";
+static const char *fonts[]          = { "DM Mono:size=10" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -72,7 +71,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_deepblue, "-sf", col_gray4, "-c" };
+//static const char *dmenucmd[] = { "/usr/bin/dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_deepblue, "-sf", col_gray4, "-c"};
+static const char *dmenucmd[] = { "/usr/bin/dmenu_run", "-i", "-c", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_deepblue, "-sf", col_gray4}; 
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *bmkscmd[]  = { "bmks", NULL };
 static const char *bmksDelcmd[]  = { "bmks", "del" };
@@ -90,9 +90,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,             		XK_q,      killclient,     {0} },
+	{ MODKEY,             		    XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -118,7 +118,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_F12,      quit,           {0} },
 };
 
 /* button definitions */
