31a8199df4bc6ced291b1e169fd938b892d55ea4
Chandler J <cjustice2000@gmail.com>
2024-11-10 15:16:19 -0700

    switch to noto sans for ui

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 24;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "DM Mono:size=10" };
+static const char *fonts[]          = { "Noto Sans:size=11" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -79,7 +79,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "/usr/bin/dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_deepblue, "-sf", col_gray4, "-c"};
-static const char *dmenucmd[] = { "dmenu_run", "-c", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_deepblue, "-sf", col_gray4}; 
+static const char *dmenucmd[] = { "dmenu_run", "-fn", "Noto Sans:size=10", "-i", "-c", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_deepblue, "-sf", col_gray4}; 
 static const char *clipmenucmd[] = { "clipmenu", "-c", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_deepblue, "-sf", col_gray4}; 
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *bmkscmd[]  = { "bmks", NULL };
