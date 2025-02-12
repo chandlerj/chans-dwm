e62172168feceb0f0a49ab3a07d65b0630ef0fab
Chandler J <cjustice2000@gmail.com>
2025-02-11 20:48:34 -0700

    gender roles for tags i guess

--- config.def.h
+++ config.def.h
@@ -40,7 +40,7 @@ static const unsigned int alphas[][3]      = {
 static const int statmonval = 0;
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5" };
+static const char *tags[] = { " ", " ", " ", " ", " " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -93,7 +93,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-c", "-i", "-fn", "Noto Sans:size=10", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_deepblue, "-sf", col_gray4, NULL }; 
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-fn", "Noto Sans:size=10", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_deepblue, "-sf", col_gray4, NULL }; 
 static const char *clipmenucmd[] = { "clipmenu", "-c", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_deepblue, "-sf", col_gray4}; 
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *bmkscmd[]  = { "bmks", NULL };
